ccedf660b59b107a0e6cadc771113b143cb6617e
Andy <andyiva00@gmail.com>
2021-11-03 15:10:20 +0300

    Add project files.

---
+++ StudentQuestionnaireForm.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace sem3_lab1
+{
+    public partial class StudentQuestionnaireForm : Form
+    {
+        public StudentQuestionnaireForm()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
